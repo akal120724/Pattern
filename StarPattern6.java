2bae4168ebd725b7db036f760f9d9ef13dfeeea0
akalyakumar <146169496+akal120724@users.noreply.github.com>
2024-07-14 19:09:08 +0530

    Create StarPattern6.java
      *     *    *   *   *

---
+++ StarPattern6.java
@@ -0,0 +1,11 @@
+class StarPattern6 {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 1; i <= 5; i++) {
+            for (j = 0; j < 5 - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print(" * ");
+        }
+    }
+}
